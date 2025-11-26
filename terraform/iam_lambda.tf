e5cc8333e068d44ca14e84513ea362092c243b6e
josé n <josenunez@MacBook-Pro-de-jose.local>
2025-11-26 16:09:02 -0600

    fix: agrega permisos de VPC al rol de lambda

--- terraform/iam_lambda.tf
+++ terraform/iam_lambda.tf
@@ -32,3 +32,9 @@ resource "aws_iam_role_policy" "lambda_sqs" {
     }]
   })
 }
+
+# --- Permiso NUEVO para que la Lambda pueda entrar a la VPC ---
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
